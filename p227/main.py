94ea703227bda2c744c2a0a05ab407632c795065
Zhijun <chenzhijun@fongwell.com>
2024-04-30 18:05:18 +0800

    debug

--- p227/main.py
+++ p227/main.py
@@ -18,34 +18,33 @@ class Solution:
         }
         expr = expr.replace(" ", "")
         getNum = 0
-        numStack = [0]
+        numStack = []
         opStack = []
+        
+        i = 0
 
-        for c in expr:
-            if c.isdigit():
-                getNum = getNum*10+int(c)
+        while i <= len(expr):
+            if i<len(expr) and expr[i].isdigit():
+                getNum = getNum*10+int(expr[i])
             else:
                 numStack.append(getNum)
                 getNum = 0
-                
-                while len(opStack)>0 and priority[opStack[-1]]>=priority[c]:
-                    b = numStack.pop()
-                    a = numStack.pop()
-                    op = opStack.pop()
-                    numStack.append(operate(a, op , b))
-                opStack.append(c)
-
-                
                     
-        numStack.append(getNum)
-        while len(opStack)>0:
-            b = numStack.pop()
-            a = numStack.pop()
-            op = opStack.pop()
-            numStack.append(operate(a, op , b))
+                if len(opStack)>0 and (i>=len(expr) or priority[opStack[-1]] >= priority[expr[i]]):
+                    while len(opStack)>0:
+                        b = numStack.pop()
+                        a = numStack.pop()
+                        op = opStack.pop()
+                        numStack.append(operate(a, op , b))
+                
+                if i<len(expr):
+                    opStack.append(expr[i])
+            i = i + 1
         return numStack[-1]
-
+                
 s = Solution()
+print(s.calculate("0"))
+print(s.calculate("1+2*5/3+6/4*2"))
 print(s.calculate("3+2*2"))
 print(s.calculate(" 3/2 "))
 print(s.calculate(" 3+5 / 2 "))
